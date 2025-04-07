2c558f162a127eff447f5e3b6b2394b670af1838
Rayal Rossetti <149338637+rayal89@users.noreply.github.com>
2025-04-07 21:53:57 +0200

    Update Progetto_parte1.sql

--- Progetto_parte1.sql
+++ Progetto_parte1.sql
@@ -1,18 +1,4 @@
-/*
-quali sono i lavori piu pagati per ruolo
-qualii sono le skills piu richieste per i lavori piu pagati
-quali sono le skill piu richieste per i ruolii
-quali sono le migliori skill in base al salrio per ruolo
-quali sono le migliori skill da imparare
-
-*/
-
-
---quali sono i lavori piu pagati per data analyst
---identificare i 10 lavori piu pagati come data analyst che sono disponibili da remoto
---focalizzati su lavori postati con specifici salari togliere i nulli
---perche 
-
+-- Quali sono i lavori piu pagati per il ruolo di Data Analyst
 
 SELECT
     job_postings_fact.*,
@@ -32,7 +18,7 @@ ORDER BY
     salary_year_avg DESC
 LIMIT 10
 
---
+
 
 
 
